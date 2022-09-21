66cf276a9ca47f8887ea7265b36be15424f4e305
Tristan <tristanobower@gmail.com>
2022-09-21 14:08:03 -0300

    folder commit

---
+++ algorithm_fundies/alpaca_data.py
@@ -0,0 +1,31 @@
+from alpaca.data import CryptoHistoricalDataClient, StockHistoricalDataClient
+from alpaca.data.requests import StockLatestQuoteRequest, CryptoBarsRequest
+from alpaca.data.historical import StockHistoricalDataClient
+from alpaca.data.timeframe import TimeFrame
+
+# no keys required.
+crypto_client = CryptoHistoricalDataClient()
+
+# keys required
+stock_client = StockHistoricalDataClient("PKG7ZA2YRCQ51BK2PY7I",  "rO2h3mj1wxol8YRfEUiM2DLL9efA3IdXQhAZP5DW")
+
+# multi symbol request - single symbol is similar
+multisymbol_request_params = StockLatestQuoteRequest(symbol_or_symbols=["SPY", "GLD", "TLT"])
+
+latest_multisymbol_quotes = stock_client.get_stock_latest_quote(multisymbol_request_params)
+
+gld_latest_ask_price = latest_multisymbol_quotes["GLD"].ask_price
+
+# no keys required for crypto data
+client = CryptoHistoricalDataClient()
+
+request_params = CryptoBarsRequest(
+                        symbol_or_symbols=["BTC/USD", "ETH/USD"],
+                        timeframe=TimeFrame.Day,
+                        start="2022-07-01T00:00:00",
+                        end="2022-07-02T00:00:00"
+                 )      
+
+bars = client.get_crypto_bars(request_params)
+
+print(bars)
