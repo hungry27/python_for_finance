5216b069d0772d99e939bbb337de16000c2f6c62
Tristan <tristanobower@gmail.com>
2022-10-05 13:03:48 -0400

    Charted apple data

--- algorithm_fundies/alpaca_data.py
+++ algorithm_fundies/alpaca_data.py
@@ -1,5 +1,8 @@
+
+import plotly.graph_objects as go
+import pandas as pd
 from alpaca.data import CryptoHistoricalDataClient, StockHistoricalDataClient
-from alpaca.data.requests import StockLatestQuoteRequest, CryptoBarsRequest
+from alpaca.data.requests import StockLatestQuoteRequest, CryptoBarsRequest, StockBarsRequest
 from alpaca.data.historical import StockHistoricalDataClient
 from alpaca.data.timeframe import TimeFrame
 
@@ -9,12 +12,13 @@ crypto_client = CryptoHistoricalDataClient()
 # keys required
 
 
+
 # multi symbol request - single symbol is similar
 multisymbol_request_params = StockLatestQuoteRequest(symbol_or_symbols=["SPY", "GLD", "TLT"])
 
-#latest_multisymbol_quotes = stock_client.get_stock_latest_quote(multisymbol_request_params)
+latest_multisymbol_quotes = stock_client.get_stock_latest_quote(multisymbol_request_params)
 
-#gld_latest_ask_price = latest_multisymbol_quotes["GLD"].ask_price
+gld_latest_ask_price = latest_multisymbol_quotes["GLD"].ask_price
 
 # no keys required for crypto data
 client = CryptoHistoricalDataClient()
@@ -29,4 +33,39 @@ request_params = CryptoBarsRequest(
 bars = client.get_crypto_bars(request_params)
 
 #print(bars)
+stock_request_params=StockBarsRequest(
+       symbol_or_symbols=["AAPL"],
+       timeframe=TimeFrame.Minute,
+       start="2022-09-20T13:30:00",
+       end="2022-09-20T21:30:00"
+)
+stock_bars = stock_client.get_stock_bars(stock_request_params)
+print(stock_bars)
+
+def extract_ohlc(stock_bars):
+
+       Date = []
+       Open =[]
+       Close =[]
+       High =[]
+       Low =[]
+
+       for entry in stock_bars['AAPL']:
+              Date.append(entry.timestamp)
+              Open.append(entry.open)
+              Close.append(entry.close)
+              High.append(entry.high)
+              Low.append(entry.low)
+
+       return Date, Open, Close, High, Low
+
+Date, Open, Close, High, Low = extract_ohlc(stock_bars)
+
+#plot bars
+fig = go.Figure(data=go.Ohlc( x = Date,
+                    open = Open,
+                    high = High,
+                    low = Low,
+                    close = Close))
+fig.show()
 
