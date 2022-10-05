279cc3836680e5c7b59b2ce533a5dd9057b59b14
Tristan <tristanobower@gmail.com>
2022-10-04 21:08:07 -0400

    pandas tutorial

--- algorithm_fundies/panda.py
+++ algorithm_fundies/panda.py
@@ -25,4 +25,23 @@ pd.Series([1,2,3,4,5])
 named_series = pd.Series ([30,35,40], index=['2015 Sales', '2016 Sales', '2017 Sales'], name = 'Product A')
 
 print (named_series)
+
+#---------------------------------Indexing, Selecting & Assigning ------------------------------------------
+
+# In pandas you can access the each column by adding a ."column_name" after the dataframe
+
+#loc and iloc are row-first, column-second. this is the opposite of what we do in native python
+
+#iloc uses the Python stdlib indexing scheme, where the first element of the range is included and the last one excluded. So 0:10 will select entries 0,...,9. loc, meanwhile, indexes inclusively. So 0:10 will select entries 0,...,10.
+
+#.iloc index-based selection
+#.loc label-based selection
+#.isin lets you select data whose values "is in" a list of values.
+#.notnull or .isnull allows me to filter out the dataset
+
 # %%
+# To get a column with iloc
+named_series.iloc[:, 0]
+named_series.iloc[:1, 0]
+
+named_series.iloc[-5:]
